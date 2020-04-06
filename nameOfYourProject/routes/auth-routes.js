dde452dc516b05d65692669a5e71fc5f444c3b2c
ASANCHEZWEB <desarrollosalbertosanchez@gmail.com>
2020-04-07 01:02:29 +0200

    social login slack y google , se necesita autorizacion de dominio para google clientsecret

--- nameOfYourProject/routes/auth-routes.js
+++ nameOfYourProject/routes/auth-routes.js
@@ -24,50 +24,80 @@ router.post("/signup", (req, res, next) => {
   }
 
   User.findOne({ username })
-  .then(user => {
-    if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
-      return;
-    }
-
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    const newUser = new User({
-      username,
-      password: hashPass
-    });
-
-    newUser.save((err) => {
-      if (err) {
-        res.render("auth/signup", { message: "Something went wrong" });
-      } else {
-        res.redirect("/");
+    .then((user) => {
+      if (user !== null) {
+        res.render("auth/signup", { message: "The username already exists" });
+        return;
       }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        password: hashPass,
+      });
+
+      newUser.save((err) => {
+        if (err) {
+          res.render("auth/signup", { message: "Something went wrong" });
+        } else {
+          res.redirect("/");
+        }
+      });
+    })
+    .catch((error) => {
+      next(error);
     });
-  })
-  .catch(error => {
-    next(error)
-  })
 });
 
 router.get("/login", (req, res, next) => {
-    res.render("auth/login", { "message": req.flash("error") });
-  });
-  
-  router.post("/login", passport.authenticate("local", {
+  res.render("auth/login", { message: req.flash("error") });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
     successRedirect: "/private-page",
     failureRedirect: "/login",
     failureFlash: true,
-    passReqToCallback: true
-  }));
+    passReqToCallback: true,
+  })
+);
 
-  router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-    res.render("private", { user: req.user });
-  });
+router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  
+  res.render("private", { user: req.user });
+});
 
-  router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/login");
-  });
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+router.get("/auth/slack", passport.authenticate("slack"));
+
+router.get("/auth/slack/callback",
+  passport.authenticate("slack", {
+    successRedirect: "/private-page",
+    failureRedirect: "/", // here you would navigate to the classic login page
+  })
+);
+
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email"
+    ]
+  })
+);
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/private-page",
+    failureRedirect: "/" // here you would redirect to the login page using traditional login approach
+  })
+);
 module.exports = router;
